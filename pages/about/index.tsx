3ad08650db93122c261f90bf8b464370c2a8786e
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2022-01-11 13:28:51 +0330

    FEAT: Add the about page

---
+++ pages/about/index.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+
+const About: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>About | Ninja App</title>
+      </Head>
+      <>
+        <h2>About</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </>
+    </>
+  );
+};
+
+export default About;
